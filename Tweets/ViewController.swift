393e6b5d7647bf1b2b44fa2acb9a7c9ca5aa1a23
Alexandre LEGENT <alexandrelegent@gmail.com>
2017-11-14 16:59:35 +0100

    Initial Commit

---
+++ Tweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweets
+//
+//  Created by Alexandre LEGENT on 14/11/2017.
+//  Copyright © 2017 Alexandre Legent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
